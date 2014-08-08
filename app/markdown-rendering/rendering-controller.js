7042f43111b2de5b94d6463ba11c25b3cab79829
azu <azuciao@gmail.com>
2014-08-08 18:07:37 +0900

    feat(view): add issue-header view

--- app/markdown-rendering/rendering-controller.js
+++ app/markdown-rendering/rendering-controller.js
@@ -3,24 +3,26 @@ var Vue = require("vue");
 var fs = require("fs");
 var marked = require("./lib/marked-nit");
 var CommentsModel = require("./Comments-model");
-var IssueHeadModel = require("./Issuehead-model");
+var RootIssueModel = require("./RootIssue-model");
 var commentsModel = new CommentsModel();
-var issueHeadModel = new IssueHeadModel();
+var rootIssue = new RootIssueModel();
 function didLoad() {
     var templatePath = __dirname + "/../template/";
     var preview = new Vue({
         el: '#editor',
         data: {
-            "issue": issueHeadModel.getRawData(),
+            "rootIssue": rootIssue.getRawData(),
             "comments": commentsModel.getRawData()
         },
         filters: {
             marked: marked
         },
         components: {
+            "issue-header": {
+                template: fs.readFileSync(templatePath + "issue-header.html", "utf-8")
+            },
             "comment": {
                 template: fs.readFileSync(templatePath + "comment.html", "utf-8")
-
             }
         }
     });
