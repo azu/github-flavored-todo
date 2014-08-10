d0dbcea143131563e08fbe49a182726d6d203c1f
azu <azuciao@gmail.com>
2014-08-10 20:21:41 +0900

    feat(issue): editable issue

--- app/issue-controller/IssueController.js
+++ app/issue-controller/IssueController.js
@@ -29,6 +29,7 @@ IssueController.prototype.loadView = function () {
     this.viewController = new Vue({
         el: '#js-main-content',
         data: {
+            "editedIssue": null,
             "editingComments": [],
             "rootIssue": rootIssue,
             "comments": []
@@ -41,7 +42,34 @@ IssueController.prototype.loadView = function () {
                 return marked(text);
             }
         },
+        directives: {
+            'todo-focus': function (value) {
+                if (!value) {
+                    return;
+                }
+                var el = this.el;
+                setTimeout(function () {
+                    el.focus();
+                }, 0);
+            }
+        },
         methods: {
+            // rootIssue
+            editTitle: function (rootIssue) {
+                this.editedIssue = rootIssue;
+                this.beforeEditCache = rootIssue.title;
+            },
+            doneEdit: function (rootIssue) {
+                if (!this.editedIssue) {
+                    return;
+                }
+                this.editedIssue = null;
+            },
+            cancelEdit: function (rootIssue) {
+                this.editedIssue = null;
+                rootIssue.title = this.beforeEditCache;
+            },
+            // comments
             isEditing: function (target) {
                 return this.editingComments.indexOf(target) !== -1;
             },
